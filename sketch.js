41fb50ec941bd2bce4663efca9aabfc6481078d2
yanyiyi <chunyan.liu@yannest.net>
2015-08-16 09:01:54 +0800

    extension to PNG
    extension to PNG

--- sketch.js
+++ sketch.js
@@ -111,7 +111,7 @@ function mousePressed() {
   }
   if (mouseX > 590 && mouseX < 960  && mouseY > 590 && mouseY < 700){ 
       
-    saveCanvas(canvas,'COSCUP10yrs'+displayName+'-'+varX, 'jpg');
+    saveCanvas(canvas,'COSCUP10yrs'+displayName+'-'+varX, 'png');
     varX++;
   }
 }
